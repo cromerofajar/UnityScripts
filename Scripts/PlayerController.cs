37fefcf69685ad1fa2bf64bbda8f4538ab0b0bea
cromerofajar <43774788+cromerofajar@users.noreply.github.com>
2019-10-28 18:17:00 +0100

    Update PlayerController.cs

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -3,13 +3,19 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+    //Panel del contador.
     public Text countText;
+    //Panel de victoria/derrota.
     public Text winText;
+    //Contador de puntos.
     private int count;
     void Start ()
     {
+        //Establece la puntuacion del contador a 0.
         count = 0;
+        //le asigna el texto.
         SetCountText ();
+        //Deja en blanco el texto de victoria/derrota.
         winText.text = "";
     }
 
@@ -18,11 +24,13 @@ public class PlayerController : MonoBehaviour {
 
     void OnTriggerEnter(Collider otro) 
     {
+        //Si se choca contra un golpeador te asigna puntos.
         if (otro.gameObject.CompareTag ("Golpeador"))
         {
             count = count + 1;
             SetCountText ();
         }
+        //En caso de llegar al final te hace perder.
         if (otro.gameObject.CompareTag("LineaFinal"))
         {
             gameObject.SetActive(false);
@@ -32,8 +40,9 @@ public class PlayerController : MonoBehaviour {
 
     void SetCountText ()
     {
-        
+        //Texto del contador con tu puntuacion.
         countText.text = "Count: " + count.ToString ();
+        //En caso de llegar a la puntuacion maxima te sale la victoria.
         if (count >= 1000)
         {
             winText.text = "Has ganado"; 
